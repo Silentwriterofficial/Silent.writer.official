fd319efe58ce82192ab60cf7ee18d7761433dfa4
Silentwriterofficial <bohorodichenko.yaroslav.24@mfkti.mk.ua>
2026-02-28 22:38:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const sidebar = document.querySelector(".sidebar");
 menuToggle.addEventListener("click", () => {
     sidebar.classList.toggle("active");
 });
+create or replace function public.increment_likes(poem_id text)
+returns bigint
+language plpgsql
+security definer
+as $$
+declare
+  new_likes bigint;
+begin
+  update public.poems
+  set likes = coalesce(likes, 0) + 1
+  where id = poem_id
+  returning likes into new_likes;
+
+  return new_likes;
+end;
+$$;
+
+grant execute on function public.increment_likes(text) to anon;
