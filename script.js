d189054f610ec1dde34cb79046493e38818d0805
Silentwriterofficial <bohorodichenko.yaroslav.24@mfkti.mk.ua>
2026-03-01 19:14:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,3 @@ if (menuToggle && sidebar) {
   menuToggle.addEventListener("click", () => {
     sidebar.classList.toggle("active");
   });
-}
-
-// ===== ПРОВЕРКА КЛИКА (временно) =====
-document.addEventListener("click", (e) => {
-  const btn = e.target.closest(".like-btn");
-  if (!btn) return;
-  alert("Кнопка работает");
-});
