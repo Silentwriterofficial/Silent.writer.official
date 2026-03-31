3806b08b91116ad9099f4fabc104614a9a43f377
Silentwriterofficial <bohorodichenko.yaroslav.24@mfkti.mk.ua>
2026-03-31 21:57:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// ===== МЕНЮ =====
 const menuToggle = document.getElementById("menuToggle");
 const sidebar = document.querySelector(".sidebar");
 
 if (menuToggle && sidebar) {
-  menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-  });
+    menuToggle.addEventListener("click", () => {
+        sidebar.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".sidebar a").forEach(link => {
+        link.addEventListener("click", () => {
+            sidebar.classList.remove("active");
+        });
+    });
+}
